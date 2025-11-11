3128e0fd6b6716f458e9997fbb32058402173a57
JRGithooberi <jere.riihilahti@gmail.com>
2025-11-11 18:11:51 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+    // kommentteja tehtävien tekemisestä
+    // ensimmäinen muutos
+    // toinen muutos
+    // kolmas muutos jne. poistetaan sitä mukaa kun tehdään
+    }
+}
