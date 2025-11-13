71d969072abe1507bd44df2f3c9c59f3c6541937
JRGithooberi <jere.riihilahti@gmail.com>
2025-11-13 18:15:46 +0200

    Järjestys on jo sekaisin
    Jere Riihilahti - päivitys

--- src/App.java
+++ src/App.java
@@ -6,9 +6,10 @@ public class App {
     // kommentteja tehtävien tekemisestä
     ensimmainenMuutos();
     // toinen muutos
+    String toinenmuutos = "Tässä on githubiin päivitys";
+    System.out.println(toinenmuutos);
     // kolmas muutos jne. poistetaan sitä mukaa kun tehdään
     }
-
 
     //Ensimmäinen muutos
     public static void ensimmainenMuutos(){
